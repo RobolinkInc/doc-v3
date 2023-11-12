3ee6ca9397ca9dfb09df90401ce34f64bab5d360
Pablo Frias <frias.pablo@gmail.com>
2023-11-12 13:23:23 -0300

    scroll for main content changed, now it's on the main content contain… (#2)
    * scroll for main content changed, now it's on the main content container and not on the body
    
    * change from let to const variables that were not being reassigned

---
+++ static/custom.js
@@ -0,0 +1,57 @@
+const waitForMenuLoadInterval = setInterval(() => {
+    const menulinks = document.querySelectorAll('a.menu__link')
+    if (menulinks.length) {
+        menulinks.forEach(function(nodo) {
+            nodo.addEventListener("click", initScrollSpy);
+        });
+        clearInterval(waitForMenuLoadInterval)
+    }
+    setTimeout(() => {
+        if (menulinks.length === 0) {
+            clearInterval(waitForMenuLoadInterval)
+        }
+    }, 10000);
+}, 200);
+
+const initScrollSpy = () => {
+    const waitForNodesLoadInterval = setInterval(() => {
+        const spyContent = document.querySelectorAll('h2.anchor, h3.anchor')
+        const spyTableItems = document.querySelectorAll('.table-of-contents a')
+        const scrollableContentContainer = document.querySelector("[class='col docItemCol_node_modules-@docusaurus-theme-classic-lib-theme-DocItem-Layout-styles-module']") 
+    
+        if (spyContent.length && spyTableItems.length && scrollableContentContainer) {
+
+            scrollableContentContainer.onscroll = (event) => {
+                spyContent.forEach(contentSec => {
+                    const top = event.target.scrollTop
+                    const offset = contentSec.offsetTop - 150
+                    const height = contentSec.offsetHeight
+                    const id = contentSec.getAttribute('id')
+    
+                    if (top === 0) {
+                        spyTableItems.forEach(links => {
+                            links.classList.remove('table-of-contents__link--active')
+                        })
+                        spyTableItems[0].classList.add('table-of-contents__link--active')
+                    } else if (top >= offset) {
+                        spyTableItems.forEach(links => {
+                            links.classList.remove('table-of-contents__link--active')
+                        })
+                        document.querySelector('.table-of-contents a[href*=' + id + ']').classList.add('table-of-contents__link--active')
+                    }
+                })
+    
+            }
+    
+    
+            clearInterval(waitForNodesLoadInterval)
+        }
+        setTimeout(() => {
+            if (spyContent.length === 0 || spyTableItems.length === 0 || !scrollableContentContainer) {
+                clearInterval(waitForMenuLoadInterval)
+            }
+        }, 10000);
+    }, 200);
+}
+
+initScrollSpy()
