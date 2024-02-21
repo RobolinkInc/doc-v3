d3fc3896c1471ca0b7d4f8e55a50347ccf20c941
fedenuche <fedenuche@gmail.com>
2024-02-21 17:19:31 -0300

    change the ga id

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,80 +1,79 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
-import remarkMath from 'remark-math';
-import rehypeKatex from 'rehype-katex';
+import remarkMath from "remark-math";
+import rehypeKatex from "rehype-katex";
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Robolink Documentation',
-  tagline: 'Robolink docs',
-  favicon: 'img/favicon.ico',
+  title: "Robolink Documentation",
+  tagline: "Robolink docs",
+  favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: 'https://staging-docs.robolink.com',
+  url: "https://staging-docs.robolink.com",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'RobolinkInc', // Usually your GitHub org/user name.
-  projectName: 'doc-v3', // Usually your repo name.
+  organizationName: "RobolinkInc", // Usually your GitHub org/user name.
+  projectName: "doc-v3", // Usually your repo name.
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
 
-   // Even if you don't use internalization, you can use this field to set useful
+  // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en', 'ko', 'ja'],
+    defaultLocale: "en",
+    locales: ["en", "ko", "ja"],
     localeConfigs: {
       en: {
-        htmlLang: 'en-GB',
+        htmlLang: "en-GB",
       },
       ko: {
-        htmlLang: 'ko-KR',
-        label: '한글 Korean', 
+        htmlLang: "ko-KR",
+        label: "한글 Korean",
       },
       ja: {
-        htmlLang: 'ja-JP',
-        label: '日本語 Japanese',
-      }
+        htmlLang: "ja-JP",
+        label: "日本語 Japanese",
+      },
     },
   },
 
   customFields: {
     prism: {
-      additionalLanguages: ['python'],
+      additionalLanguages: ["python"],
     },
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
           remarkPlugins: [remarkMath],
           rehypePlugins: [rehypeKatex],
         },
         blog: {
           showReadingTime: true,
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
         gtag: {
-          trackingID: 'G-KZR0HY6VZ9',
+          trackingID: "G-5CJZ80X5XG",
           anonymizeIP: true,
         },
-        
       }),
     ],
   ],
@@ -83,31 +82,31 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       prism: {
-        additionalLanguages: ['python'],
+        additionalLanguages: ["python"],
       },
       colorMode: {
-        defaultMode: 'dark',
+        defaultMode: "dark",
         disableSwitch: false,
         respectPrefersColorScheme: true,
       },
       navbar: {
-        style: 'dark',
+        style: "dark",
         logo: {
-          alt: 'Robolink Logo',
-          src: 'img/R-docs-logo.png',
+          alt: "Robolink Logo",
+          src: "img/R-docs-logo.png",
         },
         items: [
           {
-            type: 'localeDropdown',
-            position: 'right',
+            type: "localeDropdown",
+            position: "right",
           },
           {
-            type: 'search',
-            position: 'right',
+            type: "search",
+            position: "right",
           },
           {
-            type: 'html',
-            position: 'right',
+            type: "html",
+            position: "right",
             value: `<a role="button" id="popupBtn">Go to Lessons</a>
                     <div id="modalWrap">
                         <div id="modalContent">
@@ -138,9 +137,8 @@ const config = {
         ],
       },
       footer: {
-        style: 'dark',
-        copyright: 
-        `<div class=footer-wrap>
+        style: "dark",
+        copyright: `<div class=footer-wrap>
           <div class="footerBox" id="footerBox">
             <img id="imgFooter" src="/img/R-footer-logo.svg" />
             <span>5075 Shoreham Pl. Ste 110, San Diego, CA 92122 | +1 (858) 876-5123</span>
@@ -155,68 +153,71 @@ const config = {
         </div>`,
       },
     }),
-    plugins: [
-      [require.resolve('docusaurus-lunr-search'),{
-        languages:['en', 'ko', 'ja'],
-      }],
-      [
-        '@docusaurus/plugin-pwa',
-        {
-          pwaHead: [
-            {
-              tagName: 'link',
-              rel: 'icon',
-              href: '/img/docusaurus.png',
-            },
-            {
-              tagName: 'link',
-              rel: 'manifest',
-              href: '/manifest.json',
-            },
-            {
-              tagName: 'meta',
-              name: 'theme-color',
-              content: 'rgb(37, 194, 160)',
-            },
-            {
-              tagName: 'meta',
-              name: 'apple-mobile-web-app-capable',
-              content: 'yes',
-            },
-            {
-              tagName: 'meta',
-              name: 'apple-mobile-web-app-status-bar-style',
-              content: '#000',
-            },
-            {
-              tagName: 'link',
-              rel: 'apple-touch-icon',
-              href: '/img/docusaurus.png',
-            },
-            {
-              tagName: 'link',
-              rel: 'mask-icon',
-              href: '/img/docusaurus.svg',
-              color: 'rgb(37, 194, 160)',
-            },
-            {
-              tagName: 'meta',
-              name: 'msapplication-TileImage',
-              content: '/img/docusaurus.png',
-            },
-            {
-              tagName: 'meta',
-              name: 'msapplication-TileColor',
-              content: '#000',
-            },
-          ],
-        },
-      ],
+  plugins: [
+    [
+      require.resolve("docusaurus-lunr-search"),
+      {
+        languages: ["en", "ko", "ja"],
+      },
     ],
-    scripts: [
-      '/custom.js', 
-      'https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js',
+    [
+      "@docusaurus/plugin-pwa",
+      {
+        pwaHead: [
+          {
+            tagName: "link",
+            rel: "icon",
+            href: "/img/docusaurus.png",
+          },
+          {
+            tagName: "link",
+            rel: "manifest",
+            href: "/manifest.json",
+          },
+          {
+            tagName: "meta",
+            name: "theme-color",
+            content: "rgb(37, 194, 160)",
+          },
+          {
+            tagName: "meta",
+            name: "apple-mobile-web-app-capable",
+            content: "yes",
+          },
+          {
+            tagName: "meta",
+            name: "apple-mobile-web-app-status-bar-style",
+            content: "#000",
+          },
+          {
+            tagName: "link",
+            rel: "apple-touch-icon",
+            href: "/img/docusaurus.png",
+          },
+          {
+            tagName: "link",
+            rel: "mask-icon",
+            href: "/img/docusaurus.svg",
+            color: "rgb(37, 194, 160)",
+          },
+          {
+            tagName: "meta",
+            name: "msapplication-TileImage",
+            content: "/img/docusaurus.png",
+          },
+          {
+            tagName: "meta",
+            name: "msapplication-TileColor",
+            content: "#000",
+          },
+        ],
+      },
     ],
+  ],
+  scripts: [
+    "/custom.js",
+    "https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js",
+  ],
 };
 
 module.exports = config;
