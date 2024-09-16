a0618e7c308672a7190c25e072c52c634438d12e
green-study <soo010410@naver.com>
2024-09-16 10:49:06 -0700

    test auto publishing

--- static/custom.js
+++ static/custom.js
@@ -167,7 +167,6 @@ function loadPFRPython(pyId) {
 }
 
 window.addEventListener("message", (event) => {
-    console.log("Event added");
     if(event.origin !== "https://codrone.robolink.com"){
         return;
     }
